0656d943143e9a1155e8b5df705efcb99b7be11e
krastsislau <krastsislau@gmail.com>
2022-02-25 23:07:16 +0300

    urls for orm views added

--- companymgmt/management/urls.py
+++ companymgmt/management/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from management.views import (ListAllCompanies,
                               GetAllEmployees,
                               GetAllEmployeesFullDetails,
-                              get_all_banks)
+                              get_all_banks,
+                              get_date_company,
+                              salary_birthday_increase,
+                              create_companies,
+                              get_newest_employees)
 
 
 mgmt_urlpatterns = [
@@ -10,4 +14,10 @@ mgmt_urlpatterns = [
     path('all-employees', GetAllEmployees.as_view()),
     path('all-employees-full', GetAllEmployeesFullDetails.as_view()),
     path('all-banks', get_all_banks),
+
+    # orm task:
+    path('date-company', get_date_company),
+    path('salary-birthday-increase', salary_birthday_increase),
+    path('create-companies', create_companies),
+    path('newest-employees', get_newest_employees),
 ]
