5f0c8b5037cdc1f5aa7169b40b58285aa65bc3b9
krastsislau <krastsislau@gmail.com>
2022-04-09 22:50:27 +0300

    using dumped data from db as samples for tests now

--- companymgmt/management/conftest.py
+++ companymgmt/management/conftest.py
@@ -4,34 +4,13 @@ The home for fixtures.
 
 import pytest
 from rest_framework.test import APIClient
-from management.models import Employee, Company
+from django.core.management import call_command
 
 
-@pytest.fixture(autouse=True, scope='function')
-@pytest.mark.django_db
-def setup_and_clear_database():
-    test_company = Company(
-        name='Apple Company',
-        website='apple.com',
-        email='apple@gmail.com',
-        postcode='202020',
-    )
-    test_company.save()
-    test_employee = Employee(
-        first_name='Bob',
-        last_name='Dylan',
-        job_position='Worker',
-        is_manager=True,
-        is_admin=False,
-        phone_number='111',
-        company=Company.objects.filter(name='Apple Company').first()
-    )
-    test_employee.save()
-
-    yield
-
-    Employee.objects.filter(last_name='Dylan').delete()
-    Company.objects.filter(name='Apple Company').delete()
+@pytest.fixture(autouse=True, scope='session')
+def setup_and_clear_database(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        call_command('loaddata', 'data_samples_for_testing/samples_1.json')
 
 
 @pytest.fixture(autouse=True, scope='session')
